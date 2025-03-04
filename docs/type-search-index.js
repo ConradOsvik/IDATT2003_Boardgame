6c5e48bb6a047ac1dbc5a04b1873f9e0ffc7c8ab
ConradOsvik <55027923+ConradOsvik@users.noreply.github.com>
2025-03-04 22:19:19 +0000

    Deploying to javadoc from @ ConradOsvik/IDATT2003_Boardgame@e46b9106e2c5ee4dc4559f396e01b2b34e15c8bf 🚀

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.ntnu.stud.boardgame","l":"App"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.ntnu.stud.boardgame","l":"BoardGameApp"}];updateSearchResults();
