7b971d7fc97f9621c59c2220d91ad9a0f0581fdd
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-04-24 01:55:47 +0200

    feat: concrete board for snakes and ladders

--- src/main/java/edu/ntnu/stud/boardgame/snakesandladders/model/SLBoard.java
+++ src/main/java/edu/ntnu/stud/boardgame/snakesandladders/model/SLBoard.java
@@ -2,21 +2,186 @@ package edu.ntnu.stud.boardgame.snakesandladders.model;
 
 import edu.ntnu.stud.boardgame.core.model.Board;
 import edu.ntnu.stud.boardgame.core.model.Tile;
+import edu.ntnu.stud.boardgame.snakesandladders.model.action.LadderAction;
+import edu.ntnu.stud.boardgame.snakesandladders.model.action.SnakeAction;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implementation of the Board class for the Snakes and Ladders game.
+ * It creates a 10x10 board with 100 tiles and sets up snakes and ladders.
+ */
 public class SLBoard extends Board {
 
+  private static final int BOARD_SIZE = 10;
+  private static final int NUM_TILES = BOARD_SIZE * BOARD_SIZE;
+
+  private final Map<Integer, Integer> snakes;
+  private final Map<Integer, Integer> ladders;
+
+  /**
+   * Constructs a new Snakes and Ladders board.
+   */
+  public SLBoard() {
+    this.tiles = new HashMap<>();
+    this.snakes = new HashMap<>();
+    this.ladders = new HashMap<>();
+  }
+
+  /**
+   * Initializes the board by creating tiles and setting up snakes and ladders.
+   */
   @Override
   public void initializeBoard() {
+    for (int i = 1; i <= NUM_TILES; i++) {
+      tiles.put(i, new Tile(i));
+    }
 
+    for (int i = 1; i < NUM_TILES; i++) {
+      Tile currentTile = tiles.get(i);
+      Tile nextTile = tiles.get(i + 1);
+      currentTile.addConnectedTile(nextTile);
+    }
+
+    setupSnakesAndLadders();
   }
 
+  /**
+   * Sets up snakes and ladders on the board.
+   */
+  private void setupSnakesAndLadders() {
+    addLadder(4, 14);
+    addLadder(9, 31);
+    addLadder(20, 38);
+    addLadder(28, 84);
+    addLadder(40, 59);
+    addLadder(51, 67);
+    addLadder(63, 81);
+    addLadder(71, 91);
+
+    addSnake(17, 7);
+    addSnake(54, 34);
+    addSnake(62, 19);
+    addSnake(64, 60);
+    addSnake(87, 24);
+    addSnake(93, 73);
+    addSnake(95, 75);
+    addSnake(99, 78);
+  }
+
+  /**
+   * Adds a ladder from the start tile to the end tile.
+   *
+   * @param start The tile ID where the ladder starts
+   * @param end The tile ID where the ladder ends
+   */
+  private void addLadder(int start, int end) {
+    Tile startTile = tiles.get(start);
+    Tile endTile = tiles.get(end);
+
+    if (startTile != null && endTile != null) {
+      startTile.setLandAction(new LadderAction(endTile));
+      ladders.put(start, end);
+    }
+  }
+
+  /**
+   * Adds a snake from the start tile to the end tile.
+   *
+   * @param start The tile ID where the snake starts (head)
+   * @param end The tile ID where the snake ends (tail)
+   */
+  private void addSnake(int start, int end) {
+    Tile startTile = tiles.get(start);
+    Tile endTile = tiles.get(end);
+
+    if (startTile != null && endTile != null) {
+      startTile.setLandAction(new SnakeAction(endTile));
+      snakes.put(start, end);
+    }
+  }
+
+  /**
+   * Gets the starting tile of the game.
+   *
+   * @return The starting tile (tile ID 1)
+   */
   @Override
   public Tile getStartingTile() {
-    return null;
+    return tiles.get(1);
   }
 
+  /**
+   * Checks if the given tile is the last tile on the board.
+   *
+   * @param tile The tile to check
+   * @return True if the tile is the last tile, false otherwise
+   */
   @Override
   public boolean isLastTile(Tile tile) {
-    return false;
+    return tile != null && tile.getTileId() == NUM_TILES;
+  }
+
+  /**
+   * Gets the map of snakes on the board.
+   *
+   * @return A map where keys are the head tile IDs and values are the tail tile IDs
+   */
+  public Map<Integer, Integer> getSnakes() {
+    return new HashMap<>(snakes);
+  }
+
+  /**
+   * Gets the map of ladders on the board.
+   *
+   * @return A map where keys are the bottom tile IDs and values are the top tile IDs
+   */
+  public Map<Integer, Integer> getLadders() {
+    return new HashMap<>(ladders);
+  }
+
+  /**
+   * Gets the number of rows on the board.
+   *
+   * @return The number of rows
+   */
+  public int getRows() {
+    return BOARD_SIZE;
+  }
+
+  /**
+   * Gets the number of columns on the board.
+   *
+   * @return The number of columns
+   */
+  public int getColumns() {
+    return BOARD_SIZE;
+  }
+
+  /**
+   * Converts a tile ID to x,y coordinates on the board.
+   * The board is laid out in a snake pattern (zigzag).
+   *
+   * @param tileId The tile ID to convert
+   * @return An array where [0] is the x coordinate and [1] is the y coordinate
+   */
+  public int[] getTileCoordinates(int tileId) {
+    if (tileId < 1 || tileId > NUM_TILES) {
+      throw new IllegalArgumentException("Invalid tile ID: " + tileId);
+    }
+
+    int index = tileId - 1;
+
+    int row = BOARD_SIZE - 1 - (index / BOARD_SIZE);
+
+    int col;
+    if ((BOARD_SIZE - 1 - row) % 2 == 0) {
+      col = index % BOARD_SIZE;
+    } else {
+      col = BOARD_SIZE - 1 - (index % BOARD_SIZE);
+    }
+
+    return new int[] {col, row};
   }
 }
