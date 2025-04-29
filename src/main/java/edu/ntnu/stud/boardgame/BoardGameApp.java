7098ee9c56c32a8b8be09af5e93ee74aa4167e49
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-04-29 02:23:13 +0200

    refactor: update main class and module info

--- src/main/java/edu/ntnu/stud/boardgame/BoardGameApp.java
+++ src/main/java/edu/ntnu/stud/boardgame/BoardGameApp.java
@@ -1,52 +1,60 @@
 package edu.ntnu.stud.boardgame;
 
-import edu.ntnu.stud.boardgame.core.factory.BoardGameFactory;
-import edu.ntnu.stud.boardgame.core.model.BoardGame;
-import edu.ntnu.stud.boardgame.core.view.styles.StyleManager;
-import edu.ntnu.stud.boardgame.snakesandladders.controller.ControlPanelController;
-import edu.ntnu.stud.boardgame.snakesandladders.controller.GameBoardController;
-import edu.ntnu.stud.boardgame.snakesandladders.view.ControlPanelView;
-import edu.ntnu.stud.boardgame.snakesandladders.view.GameBoardView;
+import edu.ntnu.stud.boardgame.core.navigation.BoardGameViewControllerFactory;
+import edu.ntnu.stud.boardgame.core.navigation.Navigator;
+import edu.ntnu.stud.boardgame.core.navigation.ViewControllerFactory.ViewName;
+import edu.ntnu.stud.boardgame.core.util.StyleManager;
+import edu.ntnu.stud.boardgame.core.view.ui.Button;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-/**
- * Main application class for the Snakes and Ladders board game.
- */
 public class BoardGameApp extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    BorderPane mainLayout = new BorderPane();
+    BorderPane mainContainer = new BorderPane();
 
-    BoardGame game = BoardGameFactory.createSnakesAndLaddersGame();
-
-    GameBoardController gameBoardController = new GameBoardController(game);
-    ControlPanelController controlPanelController = new ControlPanelController(gameBoardController);
-
-    GameBoardView gameBoardView = new GameBoardView(gameBoardController);
-    ControlPanelView controlPanelView = new ControlPanelView(controlPanelController);
-
-    mainLayout.setCenter(gameBoardView);
-    mainLayout.setRight(controlPanelView);
-
-    Scene scene = new Scene(mainLayout, 900, 650);
+    Scene scene = new Scene(mainContainer, 900, 650);
 
     StyleManager.initializeBaseStyles(scene);
 
-    primaryStage.setTitle("Snakes and Ladders");
+    Navigator navigator = Navigator.getInstance();
+    navigator.setMainContainer(mainContainer);
+    navigator.setViewControllerFactory(new BoardGameViewControllerFactory());
+    navigator.setUseTransitions(true);
+
+    navigator.navigateTo(ViewName.MAIN_MENU);
+
+    ToolBar toolBar = createToolBar(navigator);
+
+    mainContainer.setTop(toolBar);
+
+    primaryStage.setTitle("Board Game");
     primaryStage.setScene(scene);
+    primaryStage.setMinWidth(800);
+    primaryStage.setMinHeight(600);
     primaryStage.setResizable(true);
     primaryStage.show();
   }
 
-  /**
-   * Main method to launch the application.
-   *
-   * @param args Command line arguments
-   */
+  private ToolBar createToolBar(Navigator navigator) {
+    ToolBar toolBar = new ToolBar();
+
+    Button goBack = Button.builder().text("Go Back").onAction(event -> {
+      navigator.goBack();
+    }).build();
+    Button exit = Button.builder().text("Exit").styleClass("danger").onAction(event -> {
+      System.exit(0);
+    }).build();
+
+    toolBar.getItems().addAll(goBack, exit);
+
+    return toolBar;
+  }
+
   public static void main(String[] args) {
     launch(args);
   }
