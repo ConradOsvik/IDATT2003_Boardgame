8cf1514f28eac35bdd504e800ac16f43796e4d30
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-04-29 02:17:25 +0200

    feat: add inter font

--- src/main/java/edu/ntnu/stud/boardgame/core/util/StyleManager.java
+++ src/main/java/edu/ntnu/stud/boardgame/core/util/StyleManager.java
@@ -1,9 +1,11 @@
-package edu.ntnu.stud.boardgame.core.view.styles;
+package edu.ntnu.stud.boardgame.core.util;
 
 import java.util.Objects;
 import javafx.scene.Scene;
 
 public class StyleManager {
+
+  private static final String BASE_CSS_PATH = "/styles/base.css";
   private static final String UI_CSS_PATH = "/styles/ui.css";
 
   public static void addStylesheet(Scene scene, String stylesheetPath) {
@@ -11,11 +13,13 @@ public class StyleManager {
       throw new IllegalArgumentException("Scene or stylesheet path cannot be null or empty");
     }
 
-    String stylesheet = Objects.requireNonNull(StyleManager.class.getResource(stylesheetPath)).toExternalForm();
+    String stylesheet = Objects.requireNonNull(StyleManager.class.getResource(stylesheetPath))
+        .toExternalForm();
     scene.getStylesheets().add(stylesheet);
   }
 
   public static void initializeBaseStyles(Scene scene) {
+    addStylesheet(scene, BASE_CSS_PATH);
     addStylesheet(scene, UI_CSS_PATH);
   }
 }
