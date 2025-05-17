6e22ba5ff6aec1cc65d670c1a0244e2284aca2fb
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-05-17 10:22:10 +0200

    wip: factory for creating boardgames

--- src/main/java/edu/ntnu/stud/boardgame/factory/BoardGameFactory.java
+++ src/main/java/edu/ntnu/stud/boardgame/factory/BoardGameFactory.java
@@ -1,5 +1,38 @@
 package edu.ntnu.stud.boardgame.factory;
 
+import edu.ntnu.stud.boardgame.exception.files.BoardFileException;
+import edu.ntnu.stud.boardgame.model.Board;
+import edu.ntnu.stud.boardgame.model.enums.BoardGameType;
+import edu.ntnu.stud.boardgame.model.game.BoardGame;
+import edu.ntnu.stud.boardgame.model.game.LadderGame;
+import edu.ntnu.stud.boardgame.service.BoardFileService;
+import java.util.List;
+
 public class BoardGameFactory {
 
+  private final BoardFileService boardFileService;
+
+  public BoardGameFactory(BoardFileService boardFileService) {
+    this.boardFileService = boardFileService;
+  }
+
+  public BoardGame createClassicLadderGame() {
+    BoardGame game = new LadderGame();
+    game.createBoard();
+    game.createDice(2);
+    return game;
+  }
+
+  public BoardGame loadGameFromFile(BoardGameType type, String fileName) throws BoardFileException {
+    Board board = boardFileService.loadBoard(fileName);
+    BoardGame game = new LadderGame();
+    game.setBoard(board);
+    game.createDice(2);
+
+    return game;
+  }
+
+  public List<String> getAvailableGameBoards() {
+    return boardFileService.listAvailableBoards();
+  }
 }
