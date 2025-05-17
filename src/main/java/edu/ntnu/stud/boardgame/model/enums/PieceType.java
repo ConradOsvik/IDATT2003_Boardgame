f35e32b34c4ed4d7bb8a9d66bb32ffd495143b40
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-05-17 22:55:00 +0200

    feat: add piece types

--- src/main/java/edu/ntnu/stud/boardgame/model/enums/PieceType.java
+++ src/main/java/edu/ntnu/stud/boardgame/model/enums/PieceType.java
@@ -1,4 +1,21 @@
 package edu.ntnu.stud.boardgame.model.enums;
 
 public enum PieceType {
+  RED("Red token"), BLUE("Blue token"), GREEN("Green token"), YELLOW("Yellow token"), BLACK(
+      "Black token");
+
+  private final String displayName;
+
+  PieceType(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  @Override
+  public String toString() {
+    return displayName;
+  }
 }
