eec814ed543da941664e6796b79b198e744275ab
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-05-18 22:04:17 +0200

    fix: fix victory screen

--- src/main/java/edu/ntnu/stud/boardgame/view/components/laddergame/VictoryScreen.java
+++ src/main/java/edu/ntnu/stud/boardgame/view/components/laddergame/VictoryScreen.java
@@ -1,75 +1,85 @@
 package edu.ntnu.stud.boardgame.view.components.laddergame;
 
+import edu.ntnu.stud.boardgame.controller.GameController;
 import edu.ntnu.stud.boardgame.model.Player;
 import edu.ntnu.stud.boardgame.view.components.builder.ButtonBuilder;
 import edu.ntnu.stud.boardgame.view.components.builder.LabelBuilder;
 import javafx.animation.FadeTransition;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
 import javafx.animation.ScaleTransition;
-import javafx.animation.Timeline;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
 
-public class VictoryScreen extends VBox {
+public class VictoryScreen extends StackPane {
 
   private final Label victoryLabel;
   private final Label winnerLabel;
   private final Label messageLabel;
+  private final Rectangle background;
+  private final GameController gameController;
 
-  public VictoryScreen() {
+  public VictoryScreen(GameController gameController) {
+    this.gameController = gameController;
+
     setAlignment(Pos.CENTER);
-    setPadding(new Insets(30));
-    setSpacing(20);
-    setMaxSize(400, 300);
 
-    Rectangle background = new Rectangle();
+    setPickOnBounds(false);
+    setMaxWidth(500);
+    setMaxHeight(400);
+    setPrefWidth(400);
+    setPrefHeight(300);
+
+    background = new Rectangle();
     background.setFill(Color.rgb(0, 0, 0, 0.7));
     background.setArcWidth(20);
     background.setArcHeight(20);
-    background.widthProperty().bind(widthProperty());
-    background.heightProperty().bind(heightProperty());
+    background.widthProperty().bind(widthProperty().subtract(40));
+    background.heightProperty().bind(heightProperty().subtract(40));
+
+    VBox contentBox = new VBox(20);
+    contentBox.setAlignment(Pos.CENTER);
+    contentBox.setPadding(new Insets(30));
+    contentBox.setMaxWidth(400);
+    contentBox.setMinWidth(300);
 
     DropShadow shadow = new DropShadow();
     shadow.setColor(Color.rgb(0, 0, 0, 0.5));
-    shadow.setRadius(10);
-    setEffect(shadow);
+    shadow.setRadius(15);
+    shadow.setOffsetX(3);
+    shadow.setOffsetY(3);
+    background.setEffect(shadow);
 
-    victoryLabel = new LabelBuilder()
-        .text("VICTORY!")
-        .styleClass("victory-title")
-        .build();
+    victoryLabel = new LabelBuilder().text("VICTORY!").styleClass("victory-title").build();
+    victoryLabel.setStyle("-fx-font-size: 36px; -fx-font-weight: bold; -fx-text-fill: gold;");
 
-    winnerLabel = new LabelBuilder()
-        .text("")
-        .styleClass("winner-label")
-        .build();
+    winnerLabel = new LabelBuilder().text("").styleClass("winner-label").build();
+    winnerLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold; -fx-text-fill: white;");
 
-    messageLabel = new LabelBuilder()
-        .text("Congratulations on winning the game!")
-        .wrapText(true)
-        .textAlignment(TextAlignment.CENTER)
-        .styleClass("victory-message")
-        .build();
+    messageLabel = new LabelBuilder().text("Congratulations on winning the game!").wrapText(true)
+        .textAlignment(TextAlignment.CENTER).styleClass("victory-message").build();
+    messageLabel.setStyle("-fx-font-size: 18px; -fx-text-fill: white;");
 
-    Button playAgainButton = new ButtonBuilder()
-        .text("Play Again")
-        .styleClass("victory-button")
-        .build();
+    Button playAgainButton = new ButtonBuilder().text("Play Again").styleClass("victory-button")
+        .onClick(e -> gameController.startGame()).build();
+    playAgainButton.setStyle(
+        "-fx-font-size: 16px; -fx-padding: 10 20 10 20; -fx-background-color: gold; "
+            + "-fx-text-fill: black;");
+
+    contentBox.getChildren().addAll(victoryLabel, winnerLabel, messageLabel, playAgainButton);
+
+    getChildren().addAll(background, contentBox);
 
     getStyleClass().add("victory-screen");
 
-    getChildren().addAll(victoryLabel, winnerLabel, messageLabel, playAgainButton);
-
-    getChildren().add(0, background);
+    setVisible(false);
   }
 
   public void showVictory(Player winner) {
@@ -79,6 +89,7 @@ public class VictoryScreen extends VBox {
       winnerLabel.setText("Game Over!");
     }
 
+    setVisible(true);
     playVictoryAnimation();
   }
 
@@ -100,14 +111,13 @@ public class VictoryScreen extends VBox {
     fadeIn.play();
     scaleUp.play();
 
-    Timeline pulseAnimation = new Timeline(
-        new KeyFrame(Duration.ZERO, new KeyValue(victoryLabel.scaleXProperty(), 1)),
-        new KeyFrame(Duration.ZERO, new KeyValue(victoryLabel.scaleYProperty(), 1)),
-        new KeyFrame(Duration.seconds(0.5), new KeyValue(victoryLabel.scaleXProperty(), 1.2)),
-        new KeyFrame(Duration.seconds(0.5), new KeyValue(victoryLabel.scaleYProperty(), 1.2)),
-        new KeyFrame(Duration.seconds(1), new KeyValue(victoryLabel.scaleXProperty(), 1))
-    );
-    pulseAnimation.setCycleCount(3);
-    pulseAnimation.play();
+    ScaleTransition pulse = new ScaleTransition(Duration.seconds(0.5), victoryLabel);
+    pulse.setFromX(1);
+    pulse.setFromY(1);
+    pulse.setToX(1.2);
+    pulse.setToY(1.2);
+    pulse.setCycleCount(6);
+    pulse.setAutoReverse(true);
+    pulse.play();
   }
 }
